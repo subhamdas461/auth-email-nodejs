40f597df198866d8215c1a1d38da997da2c6697f
Subham Das <subhamdas461@gmail.com>
2021-04-30 23:08:09 +0530

    ..

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const path = require("path");
 const signupRoute = require("./routes/SignupRoute");
 const morgan = require("morgan");
